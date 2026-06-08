95e48acfac1f320f0fddb8216ef7f4109f12d02b
Zach Archibald <ZBArchibald@gmail.com>
2026-06-08 16:32:10 -0600

    finished the position assignment in DraftState constructor, considering adding restriction logic for position values.

--- src/domain/draft_state.py
+++ src/domain/draft_state.py
@@ -1,5 +1,7 @@
 class DraftState:
-    def __init__(self, allies: list[str], enemies: list[str], banned: list[str]) -> None:
+    def __init__(self, position: str, banned: list[str], allies: list[str], enemies: list[str]) -> None:
+        self.position = position #should be one of: top, jungle, mid, adc, support
+        self.banned = banned
         self.allies = allies
         self.enemies = enemies
-        self.banned = banned
+        
